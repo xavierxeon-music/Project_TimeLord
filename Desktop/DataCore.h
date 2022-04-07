75ad6169442f9e85b81474dcb4aa039fd7cec816
Ralf Waspe <rwaspe@icloud.com>
2022-04-07 09:02:24 +0200

    upgrade core

--- Desktop/DataCore.h
+++ Desktop/DataCore.h
@@ -23,12 +23,20 @@ namespace Data
    };
    Q_ENUM_NS(Target)
 
+   struct Identifier
+   {
+      uint8_t rampIndex;
+      uint8_t stageIndex;
+
+      Identifier(const uint8_t& rampIndex = 0, const uint8_t& stageIndex = 255);
+      bool hasStage() const;
+   };
+
    struct Role
    {
-      static constexpr int RampIndex = Qt::UserRole + 11;
-      static constexpr int StageIndex = Qt::UserRole + 12;
-      static constexpr int Data = Qt::UserRole + 13;   // non integer data in original format
-      static constexpr int Target = Qt::UserRole + 14; // the target enum
+      static constexpr int Identifier = Qt::UserRole + 11; // the identifier
+      static constexpr int Target = Qt::UserRole + 12;     // the target enum
+      static constexpr int Data = Qt::UserRole + 13;       // non integer data in original format
    };
 
    enum class Type : uint8_t
@@ -41,28 +49,23 @@ namespace Data
    };
    Q_ENUM_NS(Type)
 
-   struct Identifier
-   {
-      uint8_t rampIndex;
-      uint8_t stageIndex;
-
-      Identifier(const uint8_t& rampIndex = 0, const uint8_t& stageIndex = 0);
-   };
-
    // all graph data access and manipulation should happen via this class
    class Core
    {
-
    public:
       Core(MainWidget* mainWidget);
 
    protected:
-      PolyRamp* getPolyRamp(const Identifier& identifier);
+      PolyRamp* getPolyRamp(const Identifier& identifier); // ignores stage index
+      void sendModelChanged(const Identifier& identifier);
+      virtual void modelHasChanged(const Identifier& identifier);
 
    protected:
       MainWidget* mainWidget;
       static bool lockGraphSize;
 
+   private:
+      static QList<Core*> instanceList;
    };
 
 } // namespace Data
